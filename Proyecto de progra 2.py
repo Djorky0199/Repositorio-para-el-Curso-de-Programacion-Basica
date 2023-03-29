6ab45a4f9e361f53213b68bc2ab7f34c5eba35d1
Djorky0199 <123462911+Djorky0199@users.noreply.github.com>
2023-03-29 17:57:17 -0600

    Add files via upload

---
+++ Proyecto de progra 2.py
@@ -0,0 +1,65 @@
+"""• Registro de los platillos: El encargado de cocina podrá administrar los platillos del
+restaurante segmentados por su tipo: Entradas, platos fuertes, postres y bebidas.
+Para lo cual, almacenará el sistema el tipo, descripción y precio SIN IVA.
+• Lista de órdenes: El encargado de cocina podrá visualizar todas las ordenes
+ejecutadas: Número de orden, usuario que lo solicita y la especificación de los
+platillos ordenados. Además, podrá seleccionar una orden y darla por finalizada."""
+
+
+rool = int(input("Ingrese 1 si es un Encargado \nIngrese 2 si es un Cliente\n "))
+admin = 1
+cliente = 2
+platillos = []
+ordenes = []
+#este while espara definir el encargado o el cliente
+while rool == 1:
+    
+    menu_principal = int(input("Menu principal: \n 1-Administrar platillo \n 2-Lista de ordenes \n"))
+    administrar_platillos = []
+    lista_ordenes = 2
+    for x in range(0, len(administrar_platillos)):
+        administrar_platillos.append(menu_principal)
+
+
+rool == "2"
+registro = int(input("1-Iniciar sesion \n2-Registrarse "))
+iniciar_sesion = 1
+registrarse = 2
+#este otro while es para definir si es alguien registrado o no
+while registro == 1:
+    registrado = int(input("Digite su correo electronico"))
+
+registro = 2
+correo = input("Correo electrónico: ")
+nombre = input("Nombre completo: ")
+fecha_nac = input("Fecha de nacimiento (YYYY-MM-DD): ")
+tarjeta = input("Número de tarjeta de crédito: ")
+vencimiento = input("Fecha de vencimiento de la tarjeta (MM/YY): ")
+codigo = input("Código de seguridad de la tarjeta: ")
+print("Cliente registrado")
+print ("Desea volver al menu principal? S/N")
+if cliente == S:
+    return  
+    
+
+
+
+while menu_principal == 1:
+    adm = int(input("Elija una opcion: \n 1-Entradas \n 2-Platos fuertes \n 3-Bebidas \n 4-Postre \n"))
+    if adm == 1:
+        Entradas = int(input("Ingrese su entrada"))
+    elif adm ==2:
+        plato_fuerte =int(input("Ingrese su plato fuerte"))
+    elif adm == 3:
+        bebidas = int(input("Ingrese su bebida"))
+    elif adm == 4:
+        Postre =int(input("Ingrese su postre"))
+    else:
+        print("Digite 1 para agregar mas de lo contrario digite cualquier otro numero para salir")
+
+
+    
+
+        
+    
+            
